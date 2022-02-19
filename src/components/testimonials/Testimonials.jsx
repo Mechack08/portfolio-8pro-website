f129bbd617b33b2ac4556e158503c3b4c8c9c521
8pro <mechackkobius@gmail.com>
2022-02-19 14:24:09 +0300

    Testimonials Section done, I work with the swiperjs librarie to ease the slide

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import "./testimonials.css";
+import Client from "./Client";
+import { clientReviews } from "../../data";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        className="container testiminials__container mySwiper"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {clientReviews
+          ? clientReviews.map(({ id, name, img, review }) => {
+              return (
+                <SwiperSlide key={id}>
+                  <Client img={img} name={name} review={review} />
+                </SwiperSlide>
+              );
+            })
+          : null}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonials;
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const clientReviews = [
+  {
+    id: "1",
+    name: "Manzekele Nathan",
+    img: "/img/manzekele.jpg",
+    review:
+      "J'ai vraiment ete honore du travail fait par le developpeur Kambale Mechack qu'on appelle encore #8pro. Le projet de restaurant a un design simplement magnifique et j'ai trop adore",
+  },
+  {
+    id: "2",
+    name: "Mathe Makasi",
+    img: "/img/ts.jpg",
+    review:
+      "It's really amazing when you wake up in the morning and see how amazing website you have. Nice effect, nice colors and so many features. Thanks alot #8pro. 🙏",
+  },
+  {
+    id: "3",
+    name: "Josephine Elamu",
+    img: "/img/artist-diva.jpg",
+    review:
+      "Am I suppose to say something ? I really don't think so but I would just like to say thank you for the great done Job #8pro. You made a good project for me. 💪 🍎 ❤️",
+  },
+  {
+    id: "4",
+    name: "Naomie Dada",
+    img: "/img/dorcas.jpg",
+    review:
+      "Avoir un fils developpeur ! Je crois juste que c'est un cadeau du ciel. Merci mon fils",
+  },
+];
